a5db55515fcd4f66b608e8a2ebc821a04f062191
linfengisep <wang.linfeng0213@gmail.com>
2017-12-25 23:45:40 +0100

    login interface-student-entreprise

---
+++ app/src/main/java/isep/forumisep/LoginEntreprise.java
@@ -0,0 +1,15 @@
+package isep.forumisep;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by linfengwang on 25/12/2017.
+ */
+
+public class LoginEntreprise extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_entreprise);
+    }
+}
--- app/src/main/java/isep/forumisep/MainActivity.java
+++ app/src/main/java/isep/forumisep/MainActivity.java
@@ -1,13 +1,37 @@
 package isep.forumisep;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStudient,btnEntreprise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStudient = (Button) findViewById(R.id.btnStudent);
+        btnEntreprise = (Button) findViewById(R.id.btnEntreprise);
+
+        btnStudient.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent studentIntent = new Intent(getApplicationContext(),LoginStudent.class);
+                startActivity(studentIntent);
+            }
+        });
+
+        btnEntreprise.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent entrepriseIntent = new Intent(getApplicationContext(),LoginEntreprise.class);
+                startActivity(entrepriseIntent);
+            }
+        });
     }
 }
