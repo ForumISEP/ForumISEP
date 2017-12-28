f8ec1164e45ee01a7a4a6c1dd282f0d6fbb50e1c
linfengisep <wang.linfeng0213@gmail.com>
2017-12-28 23:53:31 +0100

    SQLite

---
+++ app/src/main/java/isep/forumisep/app/AppConfig.java
@@ -0,0 +1,12 @@
+package isep.forumisep.app;
+
+/**
+ * Created by linfengwang on 28/12/2017.
+ */
+
+public class AppConfig {
+    //server user login url;
+    public static String URL_LOGIN = "http://46.193.66.28/android_login_api/login.php";
+    public static String URL_REGISTER = "http://46.193.66.28/android_login_api/register.php";
+
+}
